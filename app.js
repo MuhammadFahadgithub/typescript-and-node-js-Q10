e665e79a90548fef592e3f82b38f8fb380fc4f2f
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 15:29:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Task Q10
+//Adding Comments:Choose two of the programs you've written, and add 
+//at least one comment to each. If you don;t have anything specific to
+// write because your programs are too simple at this point, just add
+//your name and the current date at the top of each program file.Then 
+//write one sentence describing what the program does.
+//Program written by: [Muhammad Fahad]
+//current Date: [Tuesday, February 27, 2024]
+// This program will add two digits.
+//task 9: printing a favourite number.
+var favoriteNumber = 4;
+//reveling my favourite number in a message formate.
+console.log("My favourite number is ".concat(favoriteNumber, "."));
